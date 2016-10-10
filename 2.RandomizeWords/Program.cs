611e867e208c2bacf6d1cbc89d6681a0cf9f783f
elitsarangelova <elitsa.rangelova95@gmail.com>
2016-10-10 11:40:16 +0300

    new files

---
+++ 2.RandomizeWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split(' ').ToArray();
+            Random rnd = new Random();
+            
+            for (var first = 0; first< words.Length; first++)
+            {
+                var second = rnd.Next(0, words.Length);
+                var old = words[first];
+                words[first] = words[second];
+                words[second] = old;
+
+            }
+            Console.WriteLine(string.Join("\r\n", words));
+
+
+
+        }
+    }
+}
